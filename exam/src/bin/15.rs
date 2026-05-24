62e4a91c25a54f995a0095b1e93a8a79a7f753ed
e_hanwc <e_hanwc@enn.cn>
2026-05-25 07:50:52 +0800

    添加学习样例

--- exam/src/bin/15.rs
+++ exam/src/bin/15.rs
@@ -1,14 +1,22 @@
-#[derive(Debug)]
+use std::any::{Any, TypeId};
+
+#[derive(Debug, PartialEq)]
 enum Cons {
     List(i32, Box<Cons>),
     Nil,
 }
 
+fn print_cons(cons: Cons) {
+    if TypeId::of::<Cons>() == cons.type_id() {
+        println!("List");
+        return;
+    }
+    println!("Nil");
+}
 fn main() {
     let x = Cons::List(
         1,
         Box::new(Cons::List(2, Box::new(Cons::List(3, Box::new(Cons::Nil))))),
     );
-
-    println!("{:?}", x);
+    print_cons(x);
 }
